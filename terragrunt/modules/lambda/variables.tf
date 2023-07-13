5a8023e44a1fc9501447ccb70d037530182ef2c8
Aliaksei Dzemyanovich <aliaksei.dzemyanovich@gmail.com>
2023-07-13 18:53:54 +0300

    revert

--- terragrunt/modules/lambda/variables.tf
+++ terragrunt/modules/lambda/variables.tf
@@ -6,23 +6,3 @@ variable "product" {
 variable "env" {
   type = string
 }
-
-variable "PREPROD_DB_USERNAME" {
-  type    = string
-  default = ""
-}
-
-variable "PREPROD_DB_PASSWORD" {
-  type    = string
-  default = ""
-}
-
-variable "PREPROD_DB_NAME" {
-  type    = string
-  default = ""
-}
-
-variable "PREPROD_DB_HOST" {
-  type    = string
-  default = ""
-}
