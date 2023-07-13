79d678398966468a75a2a8ce5849767270179606
Aliaksei Dzemyanovich <aliaksei.dzemyanovich@gmail.com>
2023-07-13 18:42:33 +0300

    fix develop pipeline

--- terragrunt/modules/lambda/variables.tf
+++ terragrunt/modules/lambda/variables.tf
@@ -6,3 +6,23 @@ variable "product" {
 variable "env" {
   type = string
 }
+
+variable "PREPROD_DB_USERNAME" {
+  type    = string
+  default = ""
+}
+
+variable "PREPROD_DB_PASSWORD" {
+  type    = string
+  default = ""
+}
+
+variable "PREPROD_DB_NAME" {
+  type    = string
+  default = ""
+}
+
+variable "PREPROD_DB_HOST" {
+  type    = string
+  default = ""
+}
