cff87e21acddc7c8948fcadcf0ac856716f7a684
Aliaksei Dzemyanovich <aliaksei.dzemyanovich@gmail.com>
2023-08-03 14:19:01 +0300

    auth-send-code api endpoint

--- terragrunt/modules/lambda/api.tf
+++ terragrunt/modules/lambda/api.tf
@@ -13,6 +13,12 @@ resource "aws_api_gateway_resource" "get_sport_objects_api_resource" {
   path_part   = "get-sport-objects"
 }
 
+resource "aws_api_gateway_resource" "auth_send_code_api_resource" {
+  rest_api_id = aws_api_gateway_rest_api.user_api.id
+  parent_id   = aws_api_gateway_rest_api.user_api.root_resource_id
+  path_part   = "auth-send-code"
+}
+
 ############## GET get-sport-objects ##############
 
 resource "aws_api_gateway_method" "get_sport_objects_get_method" {
@@ -118,6 +124,111 @@ resource "aws_api_gateway_integration_response" "get_sport_objects_options_integ
   }
 }
 
+############## POST auth-send-code ##############
+
+resource "aws_api_gateway_method" "auth_send_code_post_method" {
+  rest_api_id   = aws_api_gateway_rest_api.user_api.id
+  resource_id   = aws_api_gateway_resource.auth_send_code_api_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "auth_send_code_post_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.user_api.id
+  resource_id             = aws_api_gateway_resource.auth_send_code_api_resource.id
+  http_method             = aws_api_gateway_method.auth_send_code_post_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS"
+  uri                     = aws_lambda_function.auth_send_code_lambda.invoke_arn
+}
+
+resource "aws_lambda_permission" "auth_send_code_lambda_permission" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.auth_send_code_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.user_api.execution_arn}/*/*"
+}
+
+resource "aws_api_gateway_method_response" "auth_send_code_method_response" {
+  rest_api_id = aws_api_gateway_rest_api.user_api.id
+  resource_id = aws_api_gateway_resource.auth_send_code_api_resource.id
+  http_method = aws_api_gateway_method.auth_send_code_post_method.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "auth_send_code_post_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.user_api.id
+  resource_id = aws_api_gateway_resource.auth_send_code_api_resource.id
+  http_method = aws_api_gateway_method.auth_send_code_post_method.http_method
+  status_code = aws_api_gateway_method_response.auth_send_code_method_response.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+}
+
+############## OPTIONS auth-send-code (for cors) ##############
+
+resource "aws_api_gateway_method" "auth_send_code_options_method" {
+  rest_api_id   = aws_api_gateway_rest_api.user_api.id
+  resource_id   = aws_api_gateway_resource.auth_send_code_api_resource.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "auth_send_code_options_integration" {
+  rest_api_id = aws_api_gateway_rest_api.user_api.id
+  resource_id = aws_api_gateway_resource.auth_send_code_api_resource.id
+  http_method = aws_api_gateway_method.auth_send_code_options_method.http_method
+  type        = "MOCK"
+
+  request_templates = {
+    "application/json" = <<EOF
+{"statusCode": 200}
+EOF
+  }
+}
+
+resource "aws_api_gateway_method_response" "auth_send_code_options_method_response" {
+  rest_api_id = aws_api_gateway_rest_api.user_api.id
+  resource_id = aws_api_gateway_resource.auth_send_code_api_resource.id
+  http_method = aws_api_gateway_method.auth_send_code_options_method.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "auth_send_code_options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.user_api.id
+  resource_id = aws_api_gateway_resource.auth_send_code_api_resource.id
+  http_method = aws_api_gateway_method.auth_send_code_options_method.http_method
+  status_code = aws_api_gateway_method_response.auth_send_code_options_method_response.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'",
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+}
+
 ############## deployment ##############
 
 resource "aws_api_gateway_deployment" "user_api_deployment" {
@@ -127,9 +238,13 @@ resource "aws_api_gateway_deployment" "user_api_deployment" {
 
   depends_on  = [
     aws_api_gateway_integration.get_sport_objects_get_integration,
-    aws_api_gateway_integration_response.get_sport_objects_get_integration_response,
     aws_api_gateway_integration.get_sport_objects_options_integration,
-    aws_api_gateway_integration_response.get_sport_objects_options_integration_response
+    aws_api_gateway_integration.auth_send_code_post_integration,
+    aws_api_gateway_integration.auth_send_code_options_integration,
+    aws_api_gateway_integration_response.get_sport_objects_get_integration_response,
+    aws_api_gateway_integration_response.get_sport_objects_options_integration_response,
+    aws_api_gateway_integration_response.auth_send_code_post_integration_response,
+    aws_api_gateway_integration_response.auth_send_code_options_integration_response
   ]
   rest_api_id = aws_api_gateway_rest_api.user_api.id
 
@@ -137,9 +252,17 @@ resource "aws_api_gateway_deployment" "user_api_deployment" {
     redeployment = sha1(jsonencode([
       aws_iam_role.iam_for_lambda.id,
       aws_api_gateway_resource.get_sport_objects_api_resource.id,
+      aws_api_gateway_resource.auth_send_code_api_resource.id,
       aws_api_gateway_method.get_sport_objects_get_method.id,
+      aws_api_gateway_method.get_sport_objects_options_method.id,
+      aws_api_gateway_method.auth_send_code_post_method.id,
+      aws_api_gateway_method.auth_send_code_options_method.id,
       aws_api_gateway_integration.get_sport_objects_get_integration.id,
-      aws_lambda_function.get_sport_objects_lambda.source_code_hash
+      aws_api_gateway_integration.get_sport_objects_options_integration.id,
+      aws_api_gateway_integration.auth_send_code_post_integration.id,
+      aws_api_gateway_integration.auth_send_code_options_integration.id,
+      aws_lambda_function.get_sport_objects_lambda.source_code_hash,
+      aws_lambda_function.auth_send_code_lambda.source_code_hash
     ]))
   }
 
