988239b4385b1a79e3bb94b862fb496f60ea68ea
Aliaksei Dzemyanovich <aliaksei.dzemyanovich@gmail.com>
2023-08-14 22:29:05 +0300

    update package.json

--- jest/setup/setup.preprod.ts
+++ jest/setup/setup.preprod.ts
@@ -1,3 +1,1 @@
-import './setup.shared';
-
 process.env.API_URL = 'https://60qhz6s8f9.execute-api.eu-central-1.amazonaws.com/preprod1';
--- jest/setup/setup.prod.ts
+++ jest/setup/setup.prod.ts
@@ -1,3 +1,1 @@
-import './setup.shared';
-
 process.env.API_URL = 'https://6nb2sgfcrl.execute-api.eu-central-1.amazonaws.com/prod1';
