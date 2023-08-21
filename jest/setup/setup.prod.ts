dedda08e07dd7f205ba721d40774fda28832af5d
Aliaksei Dzemyanovich <aliaksei.dzemyanovich@gmail.com>
2023-08-21 14:44:41 +0300

    add admin url for tests

--- jest/setup/setup.prod.ts
+++ jest/setup/setup.prod.ts
@@ -1,3 +1,2 @@
 process.env.API_URL = 'https://6nb2sgfcrl.execute-api.eu-central-1.amazonaws.com/prod1';
-// todo: add the next line
-// process.env.ADMIN_API_URL = '???';
+process.env.ADMIN_API_URL = 'https://w1ktgcl94f.execute-api.eu-central-1.amazonaws.com/prod1';
