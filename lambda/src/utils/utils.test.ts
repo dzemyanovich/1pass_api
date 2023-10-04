7fd453eec9df7141a25ff6707dc5ec0d74db654a
Aliaksei Dzemyanovich <aliaksei.dzemyanovich@gmail.com>
2023-10-04 15:09:58 +0300

    fix date comparison issue (related to time zones)

--- lambda/src/utils/utils.test.ts
+++ lambda/src/utils/utils.test.ts
@@ -1,14 +1,24 @@
-import { addDays, isToday } from './utils';
+import { addDays, compareTwoDates, isToday } from './utils';
+
+describe('compareTwoDates', () => {
+  it('returns true', () => {
+    expect(compareTwoDates('2023-10-04T14:47:50.463Z', '2023-10-03T21:00:33.000Z')).toBe(true);
+  });
+
+  it('returns false', () => {
+    expect(compareTwoDates('2023-10-04T14:47:50.463Z', '2023-10-03T20:00:33.000Z')).toBe(false);
+  });
+});
 
 describe('isToday', () => {
-  it('returns true #1', async () => {
+  it('returns true #1', () => {
     const today = new Date();
     const result = isToday(today);
 
     expect(result).toBe(true);
   });
 
-  it('returns true #2', async () => {
+  it('returns true #2', () => {
     const today = new Date();
     today.setHours(0, 0, 0, 0);
     const result = isToday(today);
@@ -16,14 +26,14 @@ describe('isToday', () => {
     expect(result).toBe(true);
   });
 
-  it('returns false because tomorrow', async () => {
+  it('returns false because tomorrow', () => {
     const tomorrow = addDays(new Date(), 1);
     const result = isToday(tomorrow);
 
     expect(result).toBe(false);
   });
 
-  it('returns false because yesterday', async () => {
+  it('returns false because yesterday', () => {
     const yesterday = addDays(new Date(), -1);
     const result = isToday(yesterday);
 
--- lambda/src/utils/utils.ts
+++ lambda/src/utils/utils.ts
@@ -1,9 +1,23 @@
 import Booking from '../db/models/booking';
 
-export function isToday(date: Date): boolean {
-  const todaysDate = new Date();
+const locale = 'en-US';
 
-  return date.setHours(0, 0, 0, 0) === todaysDate.setHours(0, 0, 0, 0);
+// function is used only for testing
+export function compareTwoDates(date1: string, date2: string, timeZone = 'Europe/Minsk'): boolean {
+  const dateString1 = new Date(date1).toLocaleDateString(locale, { timeZone });
+  const dateString2 = new Date(date2).toLocaleDateString(locale, { timeZone });
+
+  return dateString1 === dateString2;
+}
+
+export function isToday(date: Date): boolean {
+  // todo: get timeZone from sportObject instance
+  const timeZone = 'Europe/Minsk';
+  const today = new Date();
+  const todayDate = today.toLocaleDateString(locale, { timeZone });
+  const compareDate = date.toLocaleDateString(locale, { timeZone });
+
+  return todayDate === compareDate;
 }
 
 export function addDays(date: Date, days: number): Date {
