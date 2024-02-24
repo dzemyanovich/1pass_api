118dddf60ce11986d1357035f2f7b471649d221f
Aliaksei Dzemyanovich <aliaksei.dzemyanovich@gmail.com>
2024-02-24 14:52:50 +0300

    Revert "uncomment and therefore destroy sql db (temporary) because it consts  a month"
    This reverts commit 29bba3bf62ed22077c4e2da87a9888875da23a56.

--- terragrunt/modules/db/db.tf
+++ terragrunt/modules/db/db.tf
@@ -1,37 +1,36 @@
-# todo: db is commented out because it costs $20 per month
 # custom security group is required for connection from MySQL Workbench
-# resource "aws_security_group" "mysql_security_group" {
-#   // having db per each env is costly
-#   count = "${var.is_dev_env ? 1 : 0}"
-#   name  = "${var.product}-${var.env}-mysql-security-group"
-#   ingress {
-#     from_port   = 3306
-#     to_port     = 3306
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+resource "aws_security_group" "mysql_security_group" {
+  // having db per each env is costly
+  count = "${var.is_dev_env ? 1 : 0}"
+  name  = "${var.product}-${var.env}-mysql-security-group"
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
-# resource "aws_db_instance" "one_pass_db" {
-#   // having db per each env is costly
-#   count                       = "${var.is_dev_env ? 1 : 0}"
-#   allocated_storage           = 10
-#   identifier                  = "${var.product}-${var.env}-db"
-#   engine                      = "mysql"
-#   engine_version              = "5.7"
-#   instance_class              = "db.t3.micro"
-#   username                    = var.db_username
-#   password                    = var.db_password
-#   parameter_group_name        = "default.mysql5.7"
-#   # uncomment when db is created per each env
-#   # vpc_security_group_ids      = [aws_security_group.mysql_security_group.id]
-#   vpc_security_group_ids      = [aws_security_group.mysql_security_group[0].id]
-#   skip_final_snapshot         = true // makes possible deletion of db
-#   publicly_accessible         = true
-# }
+resource "aws_db_instance" "one_pass_db" {
+  // having db per each env is costly
+  count                       = "${var.is_dev_env ? 1 : 0}"
+  allocated_storage           = 10
+  identifier                  = "${var.product}-${var.env}-db"
+  engine                      = "mysql"
+  engine_version              = "5.7"
+  instance_class              = "db.t3.micro"
+  username                    = var.db_username
+  password                    = var.db_password
+  parameter_group_name        = "default.mysql5.7"
+  # uncomment when db is created per each env
+  # vpc_security_group_ids      = [aws_security_group.mysql_security_group.id]
+  vpc_security_group_ids      = [aws_security_group.mysql_security_group[0].id]
+  skip_final_snapshot         = true // makes possible deletion of db
+  publicly_accessible         = true
+}
